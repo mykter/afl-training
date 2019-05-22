0d560ec655ba9b3a14e4ff936c4c1b8e2d9e91a5
Michael Macnair <michael@macnair.org>
2019-05-22 17:44:51 +0100

    entrypoint.sh: more logging and sanity checks

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,13 @@ if [[ "$PASSMETHOD" == "env" ]]; then
         echo "env password method specified, but no password found in PASS environment variable" >&2
         exit 1
     fi
+    echo "Password set from environment variable"
 elif [[ "$PASSMETHOD" == "awsssm" ]]; then
+    if [[ -z $PASSPARAM -o -z $PASSREGION ]]; then
+        echo "awsssm password method specified, but missing PASSPARAM or PASSREGION environment variable" >&2
+        exit 1
+    fi
+    echo "Getting password from parameter $PASSPARAM"
     PASS=$(aws ssm get-parameter --name $PASSPARAM --with-decryption --region $PASSREGION | jq -r '.Parameter.Value')
 else
     echo "You must specify a method for setting the fuzzer user's password, or use a different entrypoint." >&2
@@ -16,4 +22,6 @@ else
 fi
 
 echo "fuzzer:$PASS" | chpasswd
+
+echo "Spawning SSHd"
 /usr/sbin/sshd -D
