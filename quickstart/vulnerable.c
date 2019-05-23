078554766d13de01135ed863ad7ff4eb1d89c199
Michael Macnair <michael@macnair.org>
2019-05-23 19:33:33 +0100

    quickstart: more reliably crashy

--- quickstart/vulnerable.c
+++ quickstart/vulnerable.c
@@ -3,42 +3,84 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	char input[100] = {0};
-	char *out;
+#define INPUTSIZE 100
 
-	// Slurp input
-	if (read(STDIN_FILENO, input, 100) < 0) {
-		fprintf(stderr, "Couldn't read stdin.\n");
-	}
-	if(input[0] == 'c') { 
-		// count characters
-		out = malloc(sizeof(input) - 1 + 3); // enough space for 2 digits + a space + input-1 chars
-		sprintf(out, "%lu ", strlen(input) - 1);
-		strcat(out, input+1);
+int process(char *input)
+{
+	char *out;
+	char *rest;
+	int len;
+	if (strncmp(input, "u ", 2) == 0)
+	{ // upper case command
+		char *rest;
+		len = strtol(input + 2, &rest, 10); // how many characters of the string to upper-case
+		rest += 1;							// skip the first char (should be a space)
+		out = malloc(len + strlen(input));  // could be shorter, but play it safe
+		if (len > (int)strlen(input))
+		{
+			printf("Specified length %d was larger than the input!\n", len);
+			return 1;
+		}
+		else if (out == NULL)
+		{
+			printf("Failed to allocate memory\n");
+			return 1;
+		}
+		for (int i = 0; i != len; i++)
+		{
+			out[i] = rest[i] - 32; // only handles ASCII
+		}
+		out[len] = 0;
+		strcat(out, rest + len); // append the remaining text
 		printf("%s", out);
 		free(out);
-	} else if ((input[0] == 'e') && (input[1] == 'c')) {
-		// echo input
-		printf(input + 2);
-	} else if (strncmp(input, "head", 4) == 0) {
-		// head
-		if (strlen(input) > 5) {
-			input[input[4]] = '\0'; // truncate string at specified position
-			printf("%s", input+4);
-		} else {
+	}
+	else if (strncmp(input, "head ", 5) == 0)
+	{ // head command
+		if (strlen(input) > 6)
+		{
+			len = strtol(input + 4, &rest, 10);
+			rest += 1;		  // skip the first char (should be a space)
+			rest[len] = '\0'; // truncate string at specified offset
+			printf("%s\n", rest);
+		}
+		else
+		{
 			fprintf(stderr, "head input was too small\n");
 		}
-	} else if (strcmp(input, "surprise!\n") == 0) {
-		// easter egg!
-		*(char *)1=2;
-	} else {
-		fprintf(stderr, "Usage: %s\nText utility - accepts commands on stdin and prints results to stdout:\n", argv[0]);
-		fprintf(stderr, "\tInput           | Output\n");
-		fprintf(stderr, "\t----------------+-----------------------\n");
-		fprintf(stderr, "\tec<string>      | <string> (simple echo)\n");
-		fprintf(stderr, "\thead<N><string> | The first <N> bytes of <string>\n");
-		fprintf(stderr, "\tc<string>       | The length of <string>, followed by <string>\n");
 	}
+	else if (strcmp(input, "surprise!\n") == 0)
+	{
+		// easter egg!
+		*(char *)1 = 2;
+	}
+	else
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	char *usage = "Usage: %s\n"
+				  "Text utility - accepts commands and data on stdin and prints results to stdout.\n"
+				  "\tInput             | Output\n"
+				  "\t------------------+-----------------------\n"
+				  "\tu <N> <string>    | Uppercased version of the first <N> bytes of <string>.\n"
+				  "\thead <N> <string> | The first <N> bytes of <string>.\n";
+	char input[INPUTSIZE] = {0};
+
+	// Slurp input
+	if (read(STDIN_FILENO, input, INPUTSIZE) < 0)
+	{
+		fprintf(stderr, "Couldn't read stdin.\n");
+	}
+
+	int ret = process(input);
+	if (ret)
+	{
+		fprintf(stderr, usage, argv[0]);
+	};
+	return ret;
 }
